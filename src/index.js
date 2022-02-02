8e7f0d4ca64447026eb9ce238505ab98d497567c
PolarisSchulz <abigaildisenografico@gmail.com>
2022-02-02 00:11:19 -0600

    mi primer comit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import validator from './validator.js';
-
-console.log(validator);
+import validator from "./validator.js";
+let btnvalidar = document.getElementById("botonValidarDatosCajaPrincipal");
+function validar() {
+  document.getElementById("bloqueDeBienvenida").style.display = "none";
+  document.getElementById("bloqueDeValidacion").style.display = "block"; //desocultar la parte del verificador
+  let numeroDeTarjeta = document.getElementById("creditcardnumber").value; //devuelve un nuevo objeto
+  let isValid = validator.isValid(numeroDeTarjeta);
+  let alerta = document.getElementById("alertaDeValidacion");
+  if (isValid === true) {
+    alerta.innerHTML = `<h4>Tarjeta Valida</h4>`;
+    alerta.classList.add('valid');
+  } else {
+    alerta.innerHTML = `<h4>Tarjeta Invalida</h4>`;
+    alerta.classList.add('invalid');
+  }
+  //maskify
+  let maskify = validator.maskify(numeroDeTarjeta);
+  //let maskify = validator.maskify(numeroDeTarjeta)
+  document.getElementById("cardMaskify").innerHTML = maskify;
+}
+btnvalidar.addEventListener("click", validar);
+let btnvalidar2 = document.getElementById("botonRetornarDatosCajaSecundaria");
+function validar2() {
+  document.getElementById("bloqueDeValidacion").style.display = "none";
+  document.getElementById("bloqueDeBienvenida").style.display = "block";
+}
+btnvalidar2.addEventListener("click", validar2); //Retornar a la parte uno del verificador
+//console.log(validator);//
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,40 @@
 const validator = {
-  // ...
+  //string
+  isValid: function (cardNumber) {
+    let card = Array.from(cardNumber).reverse(); // y vamos a ocupar el array from console y revertir
+    //console.log(cardNumber);
+    let sumaDeNumeros = 0;
+    let booleanoValidacion = false;
+    for (let i = 0; i < card.length; i++) {
+      //recorrer el array  blucle for
+      // console.log("for"+i)
+      if (i % 2 !== 0) {
+        card[i] = card[i] * 2; //identificar pares y nones y multiplicar por 2
+        console.log(card);
+        if (card[i] > 9) {
+          card[i] = card[i] - 9; //en los numeros mayores a 10 o iguales, restar 9
+          console.log(card[i]);
+        }
+      } else {
+        card[i] = parseInt(card[i]); //cambiar el tipo de dato
+      }
+      sumaDeNumeros = sumaDeNumeros + card[i]; //acumulador o la suma de  todos los numeros
+      console.log("+" + sumaDeNumeros);
+    }
+    if (sumaDeNumeros % 10 === 0) {
+      booleanoValidacion = true; //verdadero numero o coreecto que si se valida
+      console.log(booleanoValidacion);
+    } else {
+      booleanoValidacion = false; //falso numero o error que no se valida la tarjeta
+    }
+    console.log("resultado" + booleanoValidacion); //resultado del booleano
+    return booleanoValidacion;
+  },
+  maskify: function (mask) {
+    let maskify = mask.toString().replace(/.(?=.{4})/g, "#");
+    console.log(maskify);//es una expresion regular
+    return maskify;
+  },
 };
-
+//console.log(typeof card);
 export default validator;
